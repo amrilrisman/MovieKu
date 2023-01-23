449483dcdc1058ce28b3436986d69fbcea7069b7
amrilrisman <amrilrisman@gmail.com>
2023-01-23 15:23:51 +0700

    style add

---
+++ core/lib/style/color_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0XFF4651F2);
+const Color secondaryColor = Color(0XFFFCD849);
+const Color titleColor = Color(0XFF1F2633);
+const Color subtitleColor = Color(0XFFDADADA);
+const Color backgroundColor = Color(0XFFF4F4F4);
---
+++ features/auth/lib/domain/presentation/pages/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:core/core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  const SplashScreenPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreenPage> createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: primaryColor,
+    );
+  }
+}
